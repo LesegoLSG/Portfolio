1301f16656949fe8bf148ad5458f717f6fcdf6cf
LesegoLSG <217057620@student.uj.ac.za>
2023-09-18 08:56:15 -0500

    Working on the navigation

---
+++ src/Components/NavigationLinks/UseOnScreen.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import { useState,useEffect } from 'react';
+
+export const UseOnScreen = (ref) => {
+    const [isOnScreen, setIsOnScreen] = useState(false);
+
+    const observer = new IntersectionObserver((([entry]) =>{
+        // console.log(entry);
+        setIsOnScreen(entry.isIntersecting)
+    }),{
+        threshold:0.7
+    });
+
+    useEffect(() =>{
+        observer.observe(ref.current)
+
+        return () => observer.disconnect();
+    })
+
+  return isOnScreen;
+}
